729b5b04e6570a8f44d65237007a06b32db9755c
Devilontop <90457902+Devilontop@users.noreply.github.com>
2021-09-10 18:27:06 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,11 @@
+This is a webhooks spammer made by devil its the fastest spammer u just have to paste ur webhook url in console and it will start spamming automatically
+
+how to use this spammer
+open the console
+type 
+python main.py 
+install all the modules by commands like this pip install module name
+
+any questions in this spammer u can contact me by joining my discord server
+
+https://discord.gg/x9Ed2dwM3Q
---
+++ main.py
@@ -0,0 +1,34 @@
+import string
+import random
+import time
+import crayons
+from discord_webhook import *
+
+print("{}".format(crayons.red('use this spammer to fuck webhooks and servers')))
+print("{}".format(crayons.red('join our discord server if u like it.\n')))
+
+def send():
+    webhook = DiscordWebhook(url=webhookurl, content="@everyone https://discord.gg/impop") # Webhook url and message content
+    response = webhook.execute()
+
+# SETTINGS
+webhookurl = 'Your webhook here!'
+
+changewh = input("Do you want to change the web hook url, say Y if you haven't changed it in the file else say N. (Y/N) > ")
+if changewh == "y":
+    webhookurl = input("You have chosen to change the web hook url, paste it here and press enter. > ")
+
+amount = 0
+amount = input("How many messages would you like to be sent? ")
+print("You have chose to send "+ amount +" messages.")
+
+
+for x in range(1, int(amount) + 1):
+    code = ''.join(random.choice(string.ascii_uppercase + string.digits + string.ascii_lowercase) for _ in range(16))
+    send() 
+    print("all messages have been sent")
+    time.sleep(1.65) # waits 1.65 seconds due to ratelimits.
+
+print('\nmessages have been sent')
+time.sleep(5)
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                          
